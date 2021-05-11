0d9e00110337950e74681308eedc98826d708ae9
Mariem Aalabou <58294186+Mariem433@users.noreply.github.com>
2021-05-11 01:56:51 +0000

    Update AddEvents.java

--- src/main/java/com/mycompany/moroccovolunteer/AddEvents.java
+++ src/main/java/com/mycompany/moroccovolunteer/AddEvents.java
@@ -96,12 +96,16 @@ public class AddEvents extends HttpServlet {
         }
     }
            catch (SQLException ex) {
-           out.println("SQLException: " + ex);
+                out.println("<script type=\"text/javascript\">");
+                out.println("alert('SQL Exception" + ex +" ');");
+                out.println("location='events';");
+                out.println("</script>");
            }
             catch (Exception e) {
-            //e.printStackTrace();
-            out.println("Exception caught");
-            out.println(e.toString());
+                out.println("<script type=\"text/javascript\">");
+                out.println("alert('Exception caught" + e.toString()+" ');");
+                out.println("location='events';");
+                out.println("</script>");
             }
 
             out.println("</body>");
